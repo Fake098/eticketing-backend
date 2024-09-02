a209c34432080c23656b20efb8f3624ece6f9520
Fake098 <nikhilrathore846@gmail.com>
2024-09-02 10:52:18 +0530

    cleanup

--- routes/ticketRoutes.js
+++ routes/ticketRoutes.js
@@ -1,13 +1,8 @@
 const express = require("express");
-const {
-	purchaseTicket,
-	getTicketsForEvent,
-	getAllTickets,
-} = require("../controllers/ticketController");
+const { purchaseTicket } = require("../controllers/ticketController");
 const { protect } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
 router.post("/purchase", protect, purchaseTicket);
-router.get("/events/:eventId", protect, getTicketsForEvent);
 module.exports = router;
